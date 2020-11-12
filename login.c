24c05fcb2e92db2fcb0e50ac6b408e8a2304887f
Calvin Hunter <70008107+CalvinHunter03@users.noreply.github.com>
2020-11-12 13:56:07 -0800

    Add files via upload

--- login.c
+++ login.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
+
+struct burger
+{
+  char toppingsB[3];
+  char drinkB[3];
+  int priceB[3];
+};
+
+struct pizza
+{
+  char toppingsP[3];
+  char drinkP[3];
+  int priceP[3];
+};
+
+struct side
+{
+  char salad[3];
+  char chips[2];
+  char fruit[3];
+  int price;
+};
+
+
+
 int main()
 {
-  printf("hello");
   return 0;
 }
