a00eb35b9ba8d9abab41c9a5d46b469f8dad8752
Qogway <71851684+Qogway@users.noreply.github.com>
2020-11-12 14:18:11 -0800

    Add files via upload

--- login.c
+++ login.c
@@ -26,5 +26,25 @@ struct side
 
 int main()
 {
+  int firstChoice;
+  printf("Welcome to Our Restaurant!\nWe have a simple and strict selection for you to choose from.\n");
+  printf("Type 1 if you want a burger and 2 if you want a pizza: ");
+  scanf("%d\n", &firstChoice);
+
+  if(firstChoice = 1)
+  {
+    int topping;
+    int topping1;
+    int topping2;
+    int topping3;
+
+    printf("How many toppings do you want?(1, 2, or 3)\n");
+    scanf("%d", &topping);
+    for(int i = 0; i < topping; i++ )
+    {
+      printf("Do you want Lettuce(1), Tomato(2), or Cheese(3)? (input number)\n");
+
+    }
+  }
   return 0;
 }
